7e113e64759cec6d469df56f677aa6c076dc4f67
Muril0Pacheco <pachecomurilo04@gmail.com>
2025-05-21 15:38:42 -0300

    projeto criado

---
+++ src/main/java/com/microservico_java/mini_spotify/MiniSpotifyApplication.java
@@ -0,0 +1,13 @@
+package com.microservico_java.mini_spotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSpotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniSpotifyApplication.class, args);
+	}
+
+}
